751d848ff4013f389b53782b59fb66b8c6e2f8c4
krlan2789 <ahlipdkno.2@gmail.com>
2025-12-10 05:28:52 +0700

    Get service account from env vars . .

--- server/libs/firebase-admin.ts
+++ server/libs/firebase-admin.ts
@@ -1,11 +1,16 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../service-account.json";
 
 const adminApp = getApps().length > 0
     ? getApps().at(0)!
-    : initializeApp({ credential: cert(serviceAccount as any) });
+    : initializeApp({
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        }),
+    });
 
 const adminDb = getFirestore(adminApp);
 const auth = getAuth(adminApp);
